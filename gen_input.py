4949cf6433d1c806f4f0a0cd3b0b5b26308cdbc0
Peter <peter@staunton.ie>
2023-08-20 11:33:06 +0100

    Added type hints

--- gen_input.py
+++ gen_input.py
@@ -1,28 +1,21 @@
 import random
 
-def read_hands(filename):
-    with open(filename) as f:
+
+def read_hands(filename:str) -> list[str]:
+    with open(filename, encoding='utf-8') as f:
         return [x.strip().split(' # ') for x in f]
 
 
-
 def main():
-    hands = read_hands('input1.txt')
+    hands:List[str] = read_hands('input1.txt')
   
-    f = open('input2.txt', 'w')
+    f = open('input2.txt', 'w', encoding='utf-8')
+
     for _ in range(20):
         i, j = random.sample(range(len(hands)), 2)
-
         f.write(f'{hands[i][0]}|{hands[j][0]} # {hands[i][1]}|{hands[j][1]}\n')
 
     f.close()
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
--- poker.py
+++ poker.py
@@ -1,7 +1,7 @@
 from collections import namedtuple, Counter
 
-Card = namedtuple("Card", "num suit")
 
+Card = namedtuple("Card", "num suit")
 card_nums = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 hand_values = [
@@ -17,20 +17,20 @@ hand_values = [
     ]
 
 
-def read_games(filename):
-    with open(filename) as f:
+def read_games(filename:str) -> list[str]:
+    with open(filename, encoding='utf-8') as f:
         return [x.strip() for x in f]
 
 
-def get_cards(hand):
+def get_cards(hand:str) -> list[Card]:
     return [Card(h[:-1], h[-1]) for h in hand.split(',')]
 
 
-def sort_hand(cards):
+def sort_cards(cards:list[Card]) -> list[Card]:
     return sorted(cards, key=lambda x: card_nums.index(x.num))
 
 
-def get_hand_value(cards):
+def get_hand_value(cards:list[Card]) -> str:
     nums_counter = Counter([card.num for card in cards])
     suits_counter = Counter([card.suit for card in cards])
 
@@ -85,8 +85,8 @@ def main():
         hands = game.split(' # ')[0]
         hand1, hand2 = hands.split('|')
 
-        cards1 = sort_hand(get_cards(hand1))
-        cards2 = sort_hand(get_cards(hand2))
+        cards1 = sort_cards(get_cards(hand1))
+        cards2 = sort_cards(get_cards(hand2))
 
         hand_value1 = get_hand_value(cards1)
         hand_value2 = get_hand_value(cards2)
